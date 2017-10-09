17af0c00e723bbdd0b1519ad121d9339ffdaf1da
Jagarc88 <jagarc07@ucm.es>
2017-10-09 22:34:04 +0200

    Initial commit

---
+++ app/src/main/java/com/tfgp2p/tfg_p2p_nsp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tfgp2p.tfg_p2p_nsp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
