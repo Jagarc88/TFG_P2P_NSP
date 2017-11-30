6258b6fcdafe16c7adbc3494994e28fd94493259
Jagarc88 <jagarc07@ucm.es>
2017-11-30 19:15:23 +0100

    Añadidas

---
+++ app/src/main/java/com/tfgp2p/tfg_p2p_nsp/Fragmentos/FicherosFragment.java
@@ -0,0 +1,17 @@
+package com.tfgp2p.tfg_p2p_nsp.Fragmentos;
+
+
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Deekin on 30/11/2017.
+ */
+
+public class FicherosFragment extends FragmentTab {
+    @Override
+    public void rellenaVariables(TextView textTitutloHeadPestana, ImageView imageHeadPestana, ImageView imageExpandir, FrameLayout layoutRellenoPestana) {
+        //TODO - Rellenar con cosas chachis pistachis
+    }
+}
