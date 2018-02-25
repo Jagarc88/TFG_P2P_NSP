89acfc6664a5c4378b191df58331bb06e2306d35
Adrigarr <sgadrian@hotmail.com>
2018-02-25 20:48:39 +0100

    Revert "Limpieza de código"
    This reverts commit 467b3e8df166b7ba9a4bfd7b38e0114e0d019129.

--- app/src/main/java/com/tfgp2p/tfg_p2p_nsp/Gnutella/Echoer.java
+++ app/src/main/java/com/tfgp2p/tfg_p2p_nsp/Gnutella/Echoer.java
@@ -47,62 +47,54 @@ public class Echoer implements Runnable {
 				System.exit(-1);
 			}
 	}
-
 	public Echoer (ServerSocket s) {
 		this.tcpSocket= s;
 	}
-
 	public Echoer () {
 	
 	}
-
 	void listening(ServerSocket s){
-		try {
+			try {
+					//i++;
+					System.out.println();
 
-			System.out.println();
-
-			/*
-			 - El Echoer creado se queda bloqueado hasta que alguien quiera conectarse a él.
-			 - El AcceptInput es el que enviará los comandos/mensajes.
-			 - El UDPrecv se queda bloqueado hasta que alguien le envíe un datagrama.
-			 */
-			Thread t1 = new Thread((Runnable) new Echoer(s));
-			t1.start();
-			Thread t2= new Thread((Runnable) new AcceptInput());
-			t2.start();
-			Thread t3= new Thread((Runnable) new UDPrecv(udpSocket));
-			t3.start();
-			//start udp listening thread
-		}
-		catch (Exception e) {
-			System.out.println("Cannot accept connection");
-		}
-	}
+					/*
+					 - El Echoer creado se queda bloqueado hasta que alguien quiera conectarse a él.
+					 - El AcceptInput es el que enviará los comandos/mensajes.
+					 - El UDPrecv se queda bloqueado hasta que alguien le envíe un datagrama.
+					 */
+					Thread t1 = new Thread((Runnable) new Echoer(s));
+					t1.start();
+					Thread t2= new Thread((Runnable) new AcceptInput());
+					t2.start();
+					Thread t3= new Thread((Runnable) new UDPrecv(udpSocket));
+					t3.start();
+					//start udp listening thread
+				}
+				catch (Exception e) {
+					System.out.println("Cannot accept connection");
+				}
+			}
 	
 	public void Client(String addr, int port) 
 	{
 		try {
 			tcpClient = new Socket(addr,port);
-            //maintain a list here now to have index ipaddr, hostaddr, localport, remoteport;
 			ConnectionList.maintainList(tcpClient);
-
+			//maintain a list here now to have index ipaddr, hostaddr, localport, remoteport;
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
 	public void run() {
 		while (true) {
 			try {
 				isConnect=false;
 				Socket sock1= tcpSocket.accept();
-
 				//Client(InetAddress.getLocalHost().getHostAddress(),tcpSocket.getLocalPort());
 				System.out.println("got Conn. request from "+sock1.getInetAddress().toString());
-
 				Thread t4= new Thread ((Runnable)new Messaging(sock1));
 				t4.start();
 				//new ConnectionList(sock1);
@@ -110,9 +102,14 @@ public class Echoer implements Runnable {
 			catch (IOException o) {
 				System.out.println("Cannot connect: Read Failed");
 				}
-        }
-    }
-
+			}
+		}
+/* ***********************************************************************************
+	public static void main(String[] args) {
+		new Echoer(Integer.parseInt(args[0]),Integer.parseInt(args[1]),5);
+		
+	}
+ * ***********************************************************************************/
 }
 
 class AcceptInput implements Runnable {
@@ -124,80 +121,72 @@ class AcceptInput implements Runnable {
 	int[] cl_port=new int[20];
 	int[] cl_remote=new int[20];
 	int index=0;
-
-
 	public void run () {
+		//System.out.println("t2");
+		//try {
+			//while (true) {
+				BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+				//cmd= br.readLine();
+				///////////////////////////////////////////////////////////////////////
 
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+				cmd = "connect 192.168.0.13 1103";
+				cmdSend = "send 1 nada";
 
-        cmd = "connect 192.168.0.13 1103";
-        cmdSend = "send 1 nada";
+				///////////////////////////////////////////////////////////////////////
+				i= cmd.length();
+				tokenize(cmd,i);
 
-        i= cmd.length();
-        tokenize(cmd,i);
-
-        int j = cmdSend.length();
-        tokenize(cmdSend, j);
-
+				int j = cmdSend.length();
+				tokenize(cmdSend, j);
+				//}
+			//}
+		/*catch (IOException e) {
+				e.printStackTrace();
+		}*/
 	}
 
 	void tokenize(String cmd, int i) {
 		j=0;
 		String[] token= new String[i];
 		st= new StringTokenizer(cmd," ");
-
-
 		while (st.hasMoreElements()) {
 			token [j]=	st.nextToken();
 			j++;
 		}
-
 		if (token[0].equalsIgnoreCase("connect")) {
-
 			//token[1] es la dirección IP del cliente, token[2] su puerto.
 			ConnectionList.AcceptConn(token[1],Integer.parseInt(token[2]));
 		}
-
 		else if (token[0].equalsIgnoreCase("show")) {
 			ConnectionList.show();
 		}
-
 		else if (token[0].equalsIgnoreCase("send")) {
 			int k=2;
 			String s= "";
-
 			while (token[k] != null) {
 				s = s + token[k] + " ";
 				k++;
 			}
-
 			ConnectionList.Msg(Integer.parseInt(token[1]), s); //send()
 		}
-
 		else if (token[0].equalsIgnoreCase("sendto")) {
 			int m=3;
 			String s= "";
-
 			while (token[m] != null) {
 				s = s + token[m] + " ";
 				m++;
 			}
-
 			Messaging.send_udp(token[1], Integer.parseInt(token[2]), s);
 		}
-
 		else if (token[0].equalsIgnoreCase("disconnect")) {
 			ConnectionList.disconnect(Integer.parseInt(token[1]));
 		}
-
 		else if (token[0].equalsIgnoreCase("exit")) {
 			System.exit(-1);
 		}
-
 		else if (token[0].equalsIgnoreCase("info")) {
 			ConnectionList.info();
 		}
-
 		else {
 			System.out.println("No such Commands available, valid commands:");
 			System.out.println("info");
@@ -211,24 +200,20 @@ class AcceptInput implements Runnable {
 		}
 	}
 }
-
+	
 class Messaging implements Runnable {
 	Socket s1,s2;
 	String msg;
-
 	public Messaging (Socket s1, String msg) {
 		this.s1=s1;
 		this.msg= msg;
 		send();
 	}
-
 	public Messaging (Socket s2) {
 		this.s2=s2;
 	}
-
 	/////////////////////////////////////////////////////////////////////////////////////////////
 	/////////////////////////////////////////////////////////////////////////////////////////////
-
 	/**
 	 * Obtiene la dirección del sdcard en el dispositivo que se encuentra abierto en ese momento.
 	 * @return
@@ -249,8 +234,8 @@ class Messaging implements Runnable {
 	void send(){
 		try {
 			DataOutputStream dout= new DataOutputStream(s1.getOutputStream());
-
-
+			//dout.writeUTF(msg);
+			///////////////////////////////////////////////////////////////////////////
 			// Escribir los datos del archivo aquí.
 			String path = parseMountDirectory().getAbsolutePath() + "/de_julio.txt";
 			FileInputStream fis = new FileInputStream(path);
@@ -259,17 +244,16 @@ class Messaging implements Runnable {
 			while (fis.read(buffer) > 0) {
 				dout.write(buffer);
 			}
-
 			dout.flush();
 			fis.close();
 
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+			///////////////////////////////////////////////////////////////////////////
+			//dout.flush();
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
 	}
-
-
 	static void send_udp(String ipaddr, int udPort, String message)
 	{
 		try
@@ -278,7 +262,6 @@ class Messaging implements Runnable {
 			byte[] sendData = new byte[1024];
 			sendData = message.getBytes();
 			DatagramPacket sendPacket =  new DatagramPacket(sendData, sendData.length, InetAddress.getByName(ipaddr), udPort);
-
 			clientSocket.send(sendPacket);
 			clientSocket.close();
 		}
@@ -296,16 +279,16 @@ class Messaging implements Runnable {
 			e.printStackTrace();
 		}
 	}
-
 	public void run() {
 		boolean b=true;
-
 		while (b) {
 			DataInputStream din;
-
 			try {
 				din = new DataInputStream(s2.getInputStream()); 
-
+				/*String str= din.readUTF();
+				System.out.println(str);
+				//if (str==null) {*/
+				/////////////////////////////////////////////////////////////////
 
 				FileOutputStream fos = new FileOutputStream(parseMountDirectory().getAbsolutePath() + "/de_julio.txt");
 				byte[] buffer = new byte[4096];
@@ -322,31 +305,27 @@ class Messaging implements Runnable {
 
 				fos.close();
 
-
+				/////////////////////////////////////////////////////////////////
 				din.close();
-
+				//}
 			}
 			catch (IOException e) {
 				e.printStackTrace();
 				b=false;
-
+				//din.close();
 			}
 		}
 	}
 }
-
 
 class ConnectionList extends Thread implements Runnable {
 	static int index=0;
 	static String msg= null;
 	static Socket[] tcpClient= new Socket[20];
 	static int a=0;
-
-
 	static void AcceptConn (String ip_addr,int port) {
 		new Echoer().Client(ip_addr,port);
 	}
-
 	static void Msg (int index1, String msg1) {
 		if (tcpClient[index1] != null) {
 			new Messaging(tcpClient[index1], msg1);
@@ -355,25 +334,20 @@ class ConnectionList extends Thread implements Runnable {
 			System.out.println("Enter a valid connection id");
 		}
 	}
-
 	public ConnectionList(Socket sock3) {
 		//Echoer.isConnect= sock3.isConnected();
 		tcpClient[a]= sock3; //recvd client sock from accept now update array
 		a++;
 	}
-
 	static void maintainList(Socket sock4){
 		System.out.println("now were onset");
 		index++;
 		tcpClient[index]=sock4;
 	}
-
 	static void show() {
 		int p=1;
-
 		if (tcpClient[p] != null) {
 			System.out.println("Conn ID |	IP	|   Hostname Port |  Local Port | Remote |");
-
 			for (; p<20; p++) {
 				if (tcpClient[p] != null) {
 						System.out.println(p + "     |     "+ tcpClient[p].getInetAddress().getHostAddress() + "   |    " + tcpClient[p].getInetAddress().getHostName() + "  | " + " " + tcpClient[p].getPort() + "  | " + " " + tcpClient[p].getLocalPort() );
@@ -381,11 +355,9 @@ class ConnectionList extends Thread implements Runnable {
 			}
 		}
 	}
-
 	static void disconnect (int k) {
 		int id;
 		System.out.println("index to delete-"+k);
-
 		for (id=k; id<19; id++) {
 			try{
 				if (tcpClient[id+1] != null) {
@@ -401,11 +373,11 @@ class ConnectionList extends Thread implements Runnable {
 		}
 	index--;
 	}
-
 	static void info()
 	{
 		try
 		{
+			// String serverIp = InetAddress.getLocalHost().getHostAddress();
 			String hostName = InetAddress.getLocalHost().getHostName();
 
 			System.out.println("IP Address     |  Host Name 	         |    TCP Port  |   UDP Port");
@@ -416,38 +388,35 @@ class ConnectionList extends Thread implements Runnable {
 			e.printStackTrace();
 		}
 	}
-
 	public void run () {
 		
 	}
 }
-
-
 class UDPrecv implements Runnable 
 {
 	public DatagramSocket udpSock;
-
-	UDPrecv(DatagramSocket udpSock) throws IOException {
+	UDPrecv(DatagramSocket udpSock) throws IOException
+	{
 		this.udpSock = udpSock;
 	}
-
-	UDPrecv() {
-
+	UDPrecv()
+	{
+		
 	}
-
-	public void run() {
-
-		while(true) {
-			try {
+	public void run()
+	{
+		while(true)
+		{
+			try
+			{
 				byte[] receiveData = new byte[1024];
 				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-
 				udpSock.receive(receivePacket); 
 				String message = new String(receivePacket.getData());
-				
 				System.out.println(message);
 			}
-			catch(IOException e) {
+			catch(IOException e)
+			{
 				e.printStackTrace();
 			}
 		}
